5bdfa52f7480b62ed1fc7fff41dad20606a7728e
chitrangna <chitrangna.dev@gmail.com>
2026-07-25 23:11:38 -0700

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,17 +1,19 @@
 const fs = require("fs");
+const path = require("path");
 
-const userFile = "./users.json";
+const userFile = path.join(__dirname, "..", "users.json");
 
 let users = {};
 
 if (fs.existsSync(userFile)) {
-  users = JSON.parse(fs.readFileSync(userFile));
+  users = JSON.parse(fs.readFileSync(userFile, "utf8"));
 }
 
 function saveUsers() {
   fs.writeFileSync(
     userFile,
-    JSON.stringify(users, null, 2)
+    JSON.stringify(users, null, 2),
+    "utf8"
   );
 }
 
